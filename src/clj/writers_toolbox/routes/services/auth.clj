bde39b0591634e240cb2848f5468fa40fba7c396
Luis Borjas <luisfborjas@gmail.com>
2018-09-30 02:47:42 -0400

    incorporates validations as in the book

--- src/clj/writers_toolbox/routes/services/auth.clj
+++ src/clj/writers_toolbox/routes/services/auth.clj
@@ -1,21 +1,37 @@
 (ns writers-toolbox.routes.services.auth
   (:require [writers-toolbox.db.core :as db]
+            [writers-toolbox.validation :refer [registration-errors]]
             [ring.util.http-response :as response]
             [buddy.hashers :as hashers]
             [clojure.tools.logging :as log]))
 
-(defn register! [{:keys [session]} user]
-  (try
-    (db/create-user!
-     (-> user
-         (dissoc :pass-confirm)
-         (update :pass hashers/encrypt)))
-    (-> {:result :ok}
-        (response/ok)
-        (assoc :session (assoc session :identity (:id user))))
-    (catch Exception e
+(defn handle-registration-error [e]
+  (if (and
+       (instance? java.sql.SQLException e)
+       (-> e
+           (.getNextException)
+           (.getMessage)
+           (.startsWith "ERROR: duplicate key value")))
+    (response/precondition-failed
+     {:result :error
+      :message "User with the selected ID already exists"})
+    (do
       (log/error e)
       (response/internal-server-error
        {:result :error
-        :message "Error creating user"}))))
+        :message "Error while adding user"}))))
+
+(defn register! [{:keys [session]} user]
+  (if (registration-errors user)
+    (response/precondition-failed {:result :error})
+    (try
+      (db/create-user!
+       (-> user
+           (dissoc :pass-confirm)
+           (update :pass hashers/encrypt)))
+      (-> {:result :ok}
+          (response/ok)
+          (assoc :session (assoc session :identity (:id user))))
+      (catch Exception e
+        (handle-registration-error e)))))
 

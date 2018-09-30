aecdc3f10ce00adb70ac8c0b45901c09ca8d291a
Luis Borjas <luisfborjas@gmail.com>
2018-09-30 03:21:27 -0400

    fancy swagger conf

--- src/clj/writers_toolbox/routes/services.clj
+++ src/clj/writers_toolbox/routes/services.clj
@@ -40,40 +40,14 @@
              :data {:info {:version "1.0.0"
                            :title "Writer's Toolbox API"
                            :description "Public Services"}}}}
-  
+
+  (POST "/register" req
+        :return Result
+        :body [user UserRegistration]
+        :summary "Register a new user"
+        (auth/register! req user))
+  ; this was already here, as part of the generation
   (GET "/authenticated" []
        :auth-rules authenticated?
        :current-user user
-       (ok {:user user}))
-  (context "/api" []
-    :tags ["thingie"]
-    
-    (GET "/plus" []
-      :return       Long
-      :query-params [x :- Long, {y :- Long 1}]
-      :summary      "x+y with query-parameters. y defaults to 1."
-      (ok (+ x y)))
-
-    (POST "/minus" []
-      :return      Long
-      :body-params [x :- Long, y :- Long]
-      :summary     "x-y with body-parameters."
-      (ok (- x y)))
-
-    (GET "/times/:x/:y" []
-      :return      Long
-      :path-params [x :- Long, y :- Long]
-      :summary     "x*y with path-parameters"
-      (ok (* x y)))
-
-    (POST "/divide" []
-      :return      Double
-      :form-params [x :- Long, y :- Long]
-      :summary     "x/y with form-parameters"
-      (ok (/ x y)))
-
-    (GET "/power" []
-      :return      Long
-      :header-params [x :- Long, y :- Long]
-      :summary     "x^y with header-parameters"
-      (ok (long (Math/pow x y))))))
+       (ok {:user user})))
